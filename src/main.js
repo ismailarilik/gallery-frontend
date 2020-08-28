0bb3c18bdc4a348e6d67504083c103fe22a315b5
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-28 14:37:56 +0300

    Install and use BootstrapVue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+// Import BootstrapVue
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Use BootstrapVue
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
