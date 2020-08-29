f11dabae76a4959351464367475f896772594887
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-29 11:23:47 +0300

    Add upload buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="home">
+    <b-container fluid>
+      <b-row align-h="end">
+        <b-col cols="auto">
+          <b-form-file
+            v-model="files"
+            accept="image/*"
+            :state="Boolean(files.length)"
+            browse-text="Göz at..."
+            placeholder="Dosya seç veya buraya sürükle"
+            drop-placeholder="Dosyayı buraya sürükle"
+            :capture="true"
+            multiple>
+          </b-form-file>
+        </b-col>
+        <b-col cols="auto">
+          <b-form-file
+            v-model="file"
+            :capture="true">
+          </b-form-file>
+          <!-- <b-button>Kameradan yükle...</b-button> -->
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
