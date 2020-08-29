494d376941bf26a4b20f0612498038b6f0056cf1
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-29 19:20:29 +0300

    Write code for uploading images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,20 @@
     <b-container fluid>
       <b-row align-h="end">
         <b-col cols="auto">
-          <b-form-file
-            v-model="files"
-            :state="Boolean(files.length)"
-            browse-text="Göz at..."
-            placeholder="Dosya seç veya buraya sürükle"
-            drop-placeholder="Dosyayı buraya sürükle"
-            :capture="true"
-            multiple>
-          </b-form-file>
+          <b-form enctype="multipart/form-data">
+            <b-form-file
+              v-model="files"
+              @input="onSubmit"
+              :state="Boolean(files.length)"
+              browse-text="Göz at..."
+              placeholder="Dosya seç veya buraya sürükle"
+              drop-placeholder="Dosyayı buraya sürükle"
+              :capture="true"
+              multiple>
+            </b-form-file>
+          </b-form>
         </b-col>
+      </b-row>
       <b-row align-h="center">
         <b-col v-for="image in images" xl="2" md="3" xs="4" :key="image">
           <b-img-lazy :src="getImageUrl(image)" fluid></b-img-lazy>
@@ -36,6 +40,25 @@ export default {
       files: [],
       images: []
     }
+  },
+  methods: {
+    getImageUrl (image) {
+      return `http://localhost:3000/${image}`
+    },
+    async onSubmit () {
+      for (let i = 0; i < this.files.length; i++) {
+        const file = this.files[i]
+
+        const formData = new FormData()
+        formData.append('file', file)
+
+        try {
+          const response = await http.post('/upload', formData)
+          console.log(response)
+        } catch (err) {
+          console.log(err)
+        }
+      }
     }
   },
   async created () {
