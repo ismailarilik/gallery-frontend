0554967e2b99167bfe982fdfc646be809d1e11d0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-30 08:47:17 +0000

    Deploy ismailarilik/softtech-gallery-frontend to github.com/ismailarilik/softtech-gallery-frontend.git:gh-pages

---
+++ precache-manifest.b11abb99a59725d00a82adcd625b0eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1967d8a2a4cf34cda59",
+    "url": "/softtech-gallery-frontend/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "a1e4c3bf710d0477652cf49516f0f95f",
+    "url": "/softtech-gallery-frontend/index.html"
+  },
+  {
+    "revision": "553d0b3e4325d1849865",
+    "url": "/softtech-gallery-frontend/js/app.a24df497.js"
+  },
+  {
+    "revision": "e1967d8a2a4cf34cda59",
+    "url": "/softtech-gallery-frontend/js/chunk-vendors.5399fa20.js"
+  },
+  {
+    "revision": "88a74603fda2490dc14756e5bc31689e",
+    "url": "/softtech-gallery-frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/softtech-gallery-frontend/robots.txt"
+  }
+]);
